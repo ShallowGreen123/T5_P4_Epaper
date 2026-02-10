12caec851ffd2f50435c19a897439270db6f969a
ShallowGreen123 <2608653986@qq.com>
2026-02-10 14:29:13 +0800

    Add Screen Rotation

--- platformIO/examples/lvgl_test/main/main.cpp
+++ platformIO/examples/lvgl_test/main/main.cpp
@@ -37,90 +37,19 @@ LV_IMG_DECLARE(img_test)
 
 // 1 = enable ordered dithering (better gradients in 1bpp)
 // 0 = simple threshold
-#define EPD_ENABLE_DITHER 0
+#define EPD_ENABLE_DITHER 1
 
 // Mirror controls for LVGL flush
 // EPD_MIRROR_MODE: 0 = normal, 1 = mirror X, 2 = mirror Y, 3 = mirror X+Y (180 deg)
 #define EPD_MIRROR_MODE 1
+// EPD_ROTATION: 0, 90, 180, 270 (rotation applied when writing to panel buffer)
+#define EPD_ROTATION 0
 
 FASTEPD epaper;
 uint8_t *decodebuffer = NULL;
 uint8_t *pFramebuffer;
 // LVGL
 extern void ui_entry(void);
-
-// static void disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
-// {
-//     uint16_t w = lv_area_get_width(area) / 2;
-//     uint16_t h = lv_area_get_height(area);
-//     uint8_t *pBuffer = epaper.currentBuffer();
-
-//     Serial.printf("disp_flush w = %d, h = %d\n", w, h);
-
-// #if 1   // Mirror screen or not
-//     int w2 = w * 2;
-//     for (int i = 0; i < h; i++)
-//     {
-//         for (int j = 0; j < w2 / 2; j++)
-//         {
-//             lv_color_t t = *(color_p + (i * w2) + j);
-//             *(color_p + (i * w2) + j) = *(color_p + (i * w2) + (w2 - j - 1));
-//             *(color_p + (i * w2) + (w2 - j - 1)) = t;
-//         }
-//     }
-// #endif
-
-//     // lv_color32_t *t32 = (lv_color32_t *)color_p;
-//     // for (int i = 0; i < (w * h); i++)
-//     // {
-//     //     // 32bpp -> 8bpp (R3G3B2)
-//     //     lv_color8_t ret;
-//     //     LV_COLOR_SET_R8(ret, LV_COLOR_GET_R(*t32) >> 5); /*8 - 3  = 5*/
-//     //     LV_COLOR_SET_G8(ret, LV_COLOR_GET_G(*t32) >> 5); /*8 - 3  = 5*/
-//     //     LV_COLOR_SET_B8(ret, LV_COLOR_GET_B(*t32) >> 6); /*8 - 2  = 6*/
-//     //     pBuffer[i] = ret.full;
-//     //     t32++;
-//     // }
-
-//     lv_color16_t *c16 = (lv_color16_t *)color_p;
-//     int j = 0;
-//     for (int i = 0; i < (w*2 * h); i+=2)
-//     {
-//         // 提取 RGB565
-//         uint8_t r0 = c16[i].ch.red;
-//         uint8_t g0 = c16[i].ch.green;
-//         uint8_t b0 = c16[i].ch.blue;
-//         uint8_t r1 = c16[i + 1].ch.red;
-//         uint8_t g1 = c16[i + 1].ch.green;
-//         uint8_t b1 = c16[i + 1].ch.blue;
-
-//         // 5/6-bit -> 8-bit 扩展
-//         uint8_t r08 = (r0 << 3) | (r0 >> 2);
-//         uint8_t g08 = (g0 << 2) | (g0 >> 4);
-//         uint8_t b08 = (b0 << 3) | (b0 >> 2);
-//         uint8_t r18 = (r1 << 3) | (r1 >> 2);
-//         uint8_t g18 = (g1 << 2) | (g1 >> 4);
-//         uint8_t b18 = (b1 << 3) | (b1 >> 2);
-
-//         // 8-bit 扩展 -> 8-bit 灰度
-//         uint8_t gray0 = (r08 * 76 + g08 * 150 + b08 * 30) >> 8;
-//         uint8_t gray1 = (r18 * 76 + g18 * 150 + b18 * 30) >> 8;
-
-//         // 8-bit 灰度 -> 4-bit 灰度
-//         uint8_t g4_0 = gray0 >> 4;
-//         uint8_t g4_1 = gray1 >> 4;
-
-//         pBuffer[j++] = (g4_0 << 4) | g4_1;
-//     }
-
-//     // epaper.setRotation(0);
-//     epaper.fullUpdate(true, true);
-//     // epaper.setPasses(7);
-//     // epaper.partialUpdate(true);
-
-//     /* Inform the graphics library that you are ready with the flushing */
-//     lv_disp_flush_ready(disp);
-// }
 
 static void disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
 {
@@ -167,8 +96,21 @@ static void disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *c
             uint8_t b8 = (b << 3) | (b >> 2);
             uint8_t gray = (r8 * 76 + g8 * 150 + b8 * 30) >> 8; // 0..255
 
-            int32_t dst_x = x1 + x;
-            int32_t dst_y = y1 + y;
+            int32_t logical_x = x1 + x;
+            int32_t logical_y = y1 + y;
+            int32_t dst_x = logical_x;
+            int32_t dst_y = logical_y;
+
+#if (EPD_ROTATION == 90)
+            dst_x = (DISP_WIDTH - 1) - logical_y;
+            dst_y = logical_x;
+#elif (EPD_ROTATION == 180)
+            dst_x = (DISP_WIDTH - 1) - logical_x;
+            dst_y = (DISP_HEIGHT - 1) - logical_y;
+#elif (EPD_ROTATION == 270)
+            dst_x = logical_y;
+            dst_y = (DISP_HEIGHT - 1) - logical_x;
+#endif
 #if EPD_USE_4BPP_GRAY
             uint8_t g4 = gray >> 4; // 0..15
             int32_t idx = dst_y * pitch + (dst_x >> 1);
@@ -211,6 +153,7 @@ static void disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *c
     epaper.fullUpdate(true, true);
 #else
     epaper.partialUpdate(true, y1, y2);
+    // epaper.fullUpdate(true, true);
 #endif
     // epaper.einkPower(true);
     // or partial update: epaper.partialUpdate(true, y1, y2);
@@ -234,8 +177,13 @@ void lv_port_disp_init(void)
 
     static lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
+#if (EPD_ROTATION == 90) || (EPD_ROTATION == 270)
+    disp_drv.hor_res = DISP_HEIGHT;
+    disp_drv.ver_res = DISP_WIDTH;
+#else
     disp_drv.hor_res = DISP_WIDTH;
     disp_drv.ver_res = DISP_HEIGHT;
+#endif
     disp_drv.flush_cb = disp_flush;
     // disp_drv.render_start_cb = dips_render_start_cb;
     disp_drv.draw_buf = &draw_buf;
@@ -263,17 +211,14 @@ void idf_setup()
     rect.h = rect.w = 200;
     epaper.fullUpdate(true, false, &rect); // update only the rectangle
     delay(3000);
-
 #else
+    epaper.clearWhite(); 
     epaper.setMode(BB_MODE_1BPP);
     epaper.setPasses(7, 5);
     // epaper.setPasses(7);
-    epaper.clearWhite(); 
+    // epaper.setRotation(180);
     // delay(1000);
 #endif
-    
-    
-    // start with a white display (and buffer)
 
     lv_port_disp_init();
 
